5ab8d4c7c275f4eb32e1264a322adc90d66db9e9
Resonador <jullianbsuarez@hotmail.com>
2023-07-24 23:22:07 +0200

    añadiendo botones con estilo

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
+import React from 'react';
 import './App.css';
 import AdvertsPage from './components/adverts/advertsPage';
-
-
+import CustomButton from './components/shared/button'; // Importa el componente CustomButton
 
 function App() {
-  return  (
-  <div className="App">
+  return (
+    <div className="App">
       Welcome to Bookflip
-     <AdvertsPage />
+      <AdvertsPage />
+      <CustomButton text="Sign up" />
+      <CustomButton text="Login" /> {/* aquí lo puedo cambiar */}
     </div>
   );
 }
