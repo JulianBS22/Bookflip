edd1d473602e8b0b231ee86035e693e012e7c48d
Resonador <jullianbsuarez@hotmail.com>
2023-08-30 18:01:51 +0200

    añadiendo botones pero con error estilo

--- src/pages/AccountPage.jsx
+++ src/pages/AccountPage.jsx
@@ -1,10 +1,10 @@
 import { useContext } from "react";
 import { UserContext } from "../UserContext";
-import { Navigate } from "react-router-dom";
+import { Link, Navigate, useParams } from "react-router-dom";
 
 export default function AccountPage() {
     const {ready, user} =useContext(UserContext);
-
+    const {subpage} = useParams();
     if (!ready) {
         return 'Loading...';
     }
@@ -12,11 +12,25 @@ export default function AccountPage() {
     if (ready && !user) {
         return <Navigate to ={'/login'} />
     }
+    
+    function linkClasses (type = null) {
+        let classes = 'py-2 px-6';
+        if (type === subpage || (subpage === undefined && type === 'profile')|| type === 'search'){
+            classes += 'bg-primary text-white rounded-full';
+        
+        }
+        return classes;
+        
+    }
 
     return (
         <div>
+            <nav className="w-full flex justify-center mt-8 gap-2" >
+                <Link className = {linkClasses('profile')} to={'/account'}> My account</Link>
+                <Link className = {linkClasses('mybooks')} to={'/account/mybooks'}> My Books</Link>
+                <Link className = {linkClasses('search')} to={'/search'}> Search a book</Link>
+            </nav>
             
-            account page for {user.username} 
         </div>
     );
 }
