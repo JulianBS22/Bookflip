0fc337f19facd99a904fc9cd7e5f164e3e60d91f
Resonador <jullianbsuarez@hotmail.com>
2023-09-15 20:36:35 +0200

    arreglando logout y añadido

--- src/pages/AccountPage.jsx
+++ src/pages/AccountPage.jsx
@@ -1,36 +1,62 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { UserContext } from "../UserContext";
 import { Link, Navigate, useParams } from "react-router-dom";
+import axios from "axios";
 
 export default function AccountPage() {
-    const {ready, user} =useContext(UserContext);
-    const {subpage} = useParams();
+    const [redirect, setRedirect] = useState(null);
+    const {ready, user, setUser} =useContext(UserContext);
+    let {subpage} = useParams();
+    if (subpage === undefined) {
+        subpage = 'profile';
+    }
+
+    async function logout() {
+        await axios.post('/logout');
+        setUser(null)
+        setRedirect('/');
+    }
+
     if (!ready) {
         return 'Loading...';
     }
 
-    if (ready && !user) {
+    if (ready && !user && !redirect) {
         return <Navigate to ={'/login'} />
+
     }
+   
     
-    function linkClasses (type = null) {
-        let classes = 'py-2 px-6';
-        if (type === subpage || (subpage === undefined && type === 'profile')|| type === 'search'){
-            classes += 'bg-primary text-white rounded-full';
+    //afinar las clases aqui el hover especial no funciona
+    function linkClasses (type=null ) {
+        let classes = ' py-2 px-6 ';
+        if ( type === subpage ) {
+            classes += ' bg-primary text-white rounded-full ';
         
         }
         return classes;
         
     }
 
-    return (
+    if (redirect) 
+        return <Navigate to={redirect} />
+
+    return ( 
         <div>
-            <nav className="w-full flex justify-center mt-8 gap-2" >
-                <Link className = {linkClasses('profile')} to={'/account'}> My account</Link>
-                <Link className = {linkClasses('mybooks')} to={'/account/mybooks'}> My Books</Link>
-                <Link className = {linkClasses('search')} to={'/search'}> Search a book</Link>
+            <nav className= " w-full flex justify-center mt-8 gap-2 mb-8" >
+                <Link className = {linkClasses( 'profile' )} to={'/account'}> My account</Link>
+                <Link className = {linkClasses(' mybooks ')} to={'/account/mybooks'}> My Books</Link>
+                <Link className = {linkClasses(' search ')} to={'/search'}> Search a book</Link>
             </nav>
-            
+            {subpage === 'profile' && (
+                <div className="text-center max-w-lg mx-auto">
+                    Logged in as {user.username} ({user.email})<br />
+                    <button onClick={logout} className= "primary max-w-sm mt-2">
+                    Logout
+                    </button>
+                </div>
+                
+            )}
         </div>
     );
 }
